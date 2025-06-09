361e0862f09ea8d11347152d2f018989fd85ffed
Omaenoshi <rassvetalov.maksim@mail.ru>
2025-06-09 23:27:10 +0500

    feat: Правка

--- src/Migrator/Migrations/Migration202505151546_Create_Games_Table.cs
+++ src/Migrator/Migrations/Migration202505151546_Create_Games_Table.cs
@@ -10,8 +10,6 @@ public class Migration202505151546_Create_Games_Table : ForwardOnlyMigration
         Create.Table("games")
               .WithColumn("id").AsInt64().PrimaryKey().Identity()
               .WithColumn("player_id").AsInt64().NotNullable().ForeignKey("players", "id")
-              .WithColumn("word").AsString(128).NotNullable().ForeignKey("words", "id")
-              .WithColumn("shuffled_word").AsString(128).NotNullable()
-              .WithColumn("answer_options").AsString(int.MaxValue).NotNullable(); // JSON массив строк
+              .WithColumn("correct_word_id").AsString(128).NotNullable().ForeignKey("words", "id");
     }
 }
